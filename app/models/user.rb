e21ff9a5b4258ca7a72ad5903ea0a8738edc1840
myowin76 <myowin76@gmail.com>
2011-03-04 23:30:30 +0000

    login box ie fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :user_type_id
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me, :user_type_id
   validates :email, :presence => true, :uniqueness => true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
