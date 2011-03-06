84b9b9a4dc5575589a4ca3bf49b1dd91f263827f
myowin76 <myowin76@gmail.com>
2011-03-06 04:30:41 +0000

    s3 images test

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,11 +7,10 @@ class Photo < ActiveRecord::Base
       :styles => {	:thumb => "80x50#",
 					  :original => "346x260#"},	
 #      :url  => "/images/hotels/:id/:style/:basename.:extension",
- # 		:path => ":rails_root/public/images/hotels/:id/:style/:basename.:extension",
-      :path => "/hotels/:id/:style/:filename"
+  		:path => ":rails_root/public/images/hotels/:id/:styles/:basename.:extension",
       :storage => :s3,
-      :s3_credentials => "#{RAILS_ROOT}/config/amazon_s3.yml"
-
+      :s3_credentials => "#{RAILS_ROOT}/config/amazon_s3.yml",
+      :bucket => "worldskillsukhotels"
   		
       
       
